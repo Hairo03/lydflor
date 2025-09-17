eee1edce3aa5e95e632dec5c176cb489e9a3341e
Hairo <thehairo@gmail.com>
2025-09-17 17:05:46 +0200

    Added navbar

---
+++ app/components/Navbar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const items = ref<NavigationMenuItem[]>([
+    {
+        label: "Hjem",
+        to: "/",
+        icon: "lucide:house"
+    },
+    {
+        label: "Program",
+        to: "/program",
+        icon: "lucide:calendar"
+    },
+    {
+        label: "Biletter",
+        to: "/biletter",
+        icon: "lucide:ticket"
+    }
+])
+
+</script>
+<template>
+    <UNavigationMenu :items="items" class="w-full justify-center" />
+</template>
